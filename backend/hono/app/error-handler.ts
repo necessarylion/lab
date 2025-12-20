707f6ec02cc2a8085c58b41929058b521f8b5d98
necessarylion <aj.zinkyaw@gmail.com>
2025-12-21 01:27:37 +0700

    validation error should return with 422 status

--- backend/hono/app/error-handler.ts
+++ backend/hono/app/error-handler.ts
@@ -2,15 +2,24 @@ import { errors } from "@vinejs/vine"
 import type { Context } from "hono"
 
 export default function errorHandler(err: Error, c: Context) {
+	// validation error
 	if (err instanceof errors.E_VALIDATION_ERROR) {
-		return c.json({
-			success: false,
-			message: err.message,
-			errors: err.messages,
-		})
+		return c.json(
+			{
+				success: false,
+				message: err.message,
+				errors: err.messages,
+			},
+			422,
+		)
 	}
+
+	// all other errors
 	return c.json(
-		{ success: false, message: err.message || "Internal server error" },
+		{
+			success: false,
+			message: err.message || "Internal server error",
+		},
 		500,
 	)
 }
