036ec9b0888259c02c6ffcd08ac17695802871cb
necessarylion <aj.zinkyaw@gmail.com>
2025-12-20 16:49:18 +0700

    return meta data in prisma error

--- backend/hono/src/error-handler.ts
+++ backend/hono/src/error-handler.ts
@@ -4,7 +4,12 @@ import { Prisma } from "./generated/prisma/client"
 export default function errorHandler(err: Error, c: Context) {
 	if (err instanceof Prisma.PrismaClientKnownRequestError) {
 		return c.json(
-			{ success: false, message: err.message, prismaErrorCode: err.code },
+			{
+				success: false,
+				message: err.message,
+				prismaErrorCode: err.code,
+				meta: err.meta
+			},
 			400,
 		)
 	}
