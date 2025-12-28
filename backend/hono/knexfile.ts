6f491026cb73e358d64e197bbf0569298bd04256
necessarylion <aj.zinkyaw@gmail.com>
2025-12-28 08:51:10 +0700

    remove-hono

--- backend/hono/knexfile.ts
+++
@@ -1,9 +0,0 @@
-import type { Knex } from "knex"
-
-const config: Knex.Config = {
-	client: "mysql2",
-	connection: process.env.DATABASE_URL,
-	pool: { min: 1, max: 10 },
-}
-
-module.exports = config
--- backend/hono/routes/web.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono"
-import WebController from "@/app/controllers/web_controller"
-import { C } from "@/utils"
-
-const route = new Hono()
-
-route.get("/", C(WebController, "home"))
-
-export default route
